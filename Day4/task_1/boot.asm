cb423d6502381c7716c8c555869a147abd8a9b29
Balasaraswathy B <133559322+Balasaraswathy27@users.noreply.github.com>
2025-12-23 11:46:43 +0530

    Update boot.asm
    Program waits for key
    
    If ESC (scan code) → exit (hang)
    
    If BACKSPACE:
    
    delete previous character on screen
    
    do nothing if buffer is empty
    
    Otherwise:
    
    echo the character
    
    store it in buffer
    
    increment length

--- Day4/task_1/boot.asm
+++ Day4/task_1/boot.asm
@@ -1,1 +1,43 @@
+BITS 16           ; 16-bit code for BIOS
+ORG 0x7C00        ; BIOS loads boot sector here
 
+start:
+    xor cx, cx          ; length counter = 0
+    mov di, buffer      ; DI points to buffer
+
+read_loop:
+    mov ah, 0x00
+    int 0x16            ; wait for key, AL = ASCII
+
+    cmp ah,0x01
+    je hang
+
+    cmp al, 0x08       ; backspace pressed?
+    je backspace
+
+    stosb               ; store AL in buffer[DI], DI++
+    inc cx              ; increment length counter
+
+    mov ah, 0x0E
+    int 0x10            ; echo character
+    jmp read_loop
+
+backspace:
+    cmp cx,0
+    je read_loop
+    mov ah,0x0E
+    mov al,0x08
+    int 0x10
+    mov al,' '
+    int 0x10
+    mov al,0x08
+    int 0x10
+    dec cx
+    dec di
+    jmp read_loop
+hang:
+        jmp $
+
+buffer db 32 dup(0)
+TIMES 510-($-$$) db 0  ; pad to 510 bytes
+DW 0xAA55              ; boot signature
